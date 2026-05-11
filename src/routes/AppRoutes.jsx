bbb0b7ca57bab879df3e2231288a70cf4b515d40
Samuel Ramos <samuel.ramos@upb.edu.co>
2026-05-11 11:59:53 -0500

    refactor: centralizar rutas en AppRoutes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Layout from "../components/templates/Layout";
+
+import Gallery from "../components/organisms/gallery/Gallery";
+import Login from "../components/organisms/login/Login";
+import Register from "../components/organisms/register/Register";
+import Profile from "../components/organisms/profile/Profile";
+import ProductDetail from "../components/organisms/productDetail/ProductDetail";
+import Cart from "../components/organisms/cart/Cart";
+import Checkout from "../components/organisms/checkout/Checkout";
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Navigate to="/gallery" replace />} />
+                <Route path="gallery" element={<Gallery />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="product/:id" element={<ProductDetail />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="checkout" element={<Checkout />} />
+            </Route>
+        </Routes>
+    );
+}
